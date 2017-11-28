f094aaf78872ce960d9a07566793a698e9164552
luis1612 <ibarguen1294@gmail.com>
2017-11-28 14:54:59 -0500

    Add files via upload

---
+++ snake/snake3/Comida.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake3
+{
+    class Comida : objeto
+    {
+        public Comida()
+        {
+            this.x = generar(78);
+            this.y = generar(39);
+        }
+        public void dibujar(Graphics g)
+        {
+            g.FillRectangle(new SolidBrush(Color.Red), this.x, this.y, this.ancho, this.ancho);
+        }
+        public void colocar()
+        {
+            this.x = generar(78);
+            this.y = generar(39);
+        }
+        public int generar(int n)
+        {
+            Random random = new Random();
+            int num = random.Next(0, n) * 10;
+            return num;
+        }
+    }
+}
---
+++ snake/snake3/objeto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace snake3
+{
+    class objeto
+    {
+        protected int x, y, ancho;
+        public objeto()
+        {
+            ancho = 10;
+        }
+        public Boolean interseccion(objeto o)
+        {
+            int difx = Math.Abs(this.x - o.x);
+            int dify = Math.Abs(this.y - o.y);
+            if(difx >= 0 && difx < ancho && dify >= 0 && dify < ancho)
+            {
+                return true;
+            } else
+            {
+                return false;
+            }
+        }
+    }
+}
